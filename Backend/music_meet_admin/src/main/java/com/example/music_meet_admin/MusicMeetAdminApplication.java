d66347f31db27b315c6db22b0cfbbd2fd9102fc3
ycs-202007055 <abc50050@naver.com>
2022-09-27 21:29:50 +0900

    add: branch 생성

---
+++ Backend/music_meet_admin/src/main/java/com/example/music_meet_admin/MusicMeetAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.music_meet_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicMeetAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicMeetAdminApplication.class, args);
+	}
+
+}
---
+++ Backend/music_meet_admin/src/main/java/com/example/music_meet_admin/Service/EncryptionService.java
@@ -0,0 +1,12 @@
+package com.example.music_meet_admin.Service;
+
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EncryptionService
+{
+    BCryptPasswordEncoder
+
+}
