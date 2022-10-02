9d6bb652507bd4476a7ad7aaa4b057a428b119b9
AntenA <80020067+ycs-202007055@users.noreply.github.com>
2022-10-02 23:45:55 +0900

    dsa

--- Backend/src/main/java/com/example/music_meet_admin/JPA/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.music_meet_admin.JPA;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface AdminRepository extends JpaRepository<Admin, Long> //  JpaRepository<객체, @Id 타입>
-{
-    
-}
