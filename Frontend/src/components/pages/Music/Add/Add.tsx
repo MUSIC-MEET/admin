80f82190a8d5f1a032ab1641fa90a9dd49b93fbc
dobidugi <dobidugi@gmail.com>
2022-10-19 14:15:20 +0900

    음악 등록시 Form reset

--- Frontend/src/components/pages/Music/Add/Add.tsx
+++ Frontend/src/components/pages/Music/Add/Add.tsx
@@ -1,16 +1,45 @@
 import Title from "components/common/Title";
 import ArticleWrapper from "components/UI/ArticleWrapper";
-import React from "react";
+import React, { useState } from "react";
 import MusicAddForm from "./MusicAddForm";
 import { useMutation } from "react-query";
 import addMusic from "../../../../utils/Musics/addMusic";
+import MusicTextInfoType from "../MusicTextInfoType";
 
 /**
  * 음악 추가 페이지
  * @returns 
  */
 function Add() {
-    const { mutate } = useMutation(["addMusic"], addMusic);
+    const [albumMp3File, setAlbumMp3File] = useState<Blob>(new Blob());
+    const [albumImgFile, setAlbumImgFile] = useState<Blob>(new Blob());
+    const [imgPreview, setImgPreview] = useState<string>("");
+    const [textInfo, setTextInfo] = useState<
+        MusicTextInfoType
+    >({
+        origin_title: "",
+        origin_singer: "",
+        releaseDate: "",
+        lyrics: "",
+        genre: "",
+    });
+    const { mutate } = useMutation(["addMusic"], addMusic, {
+        onSuccess: (response) => {
+            if (response.status === 201) {
+                setAlbumMp3File(() => new Blob());
+                setAlbumImgFile(() => new Blob());
+                setImgPreview(() => "");
+                setTextInfo({
+                    origin_title: "",
+                    origin_singer: "",
+                    releaseDate: "",
+                    lyrics: "",
+                    genre: "",
+                });
+                alert("등록 완료");
+            }
+        }
+    });
 
     return (
         <React.Fragment>
@@ -18,6 +47,14 @@ function Add() {
             <ArticleWrapper>
                 <MusicAddForm
                     onSend={mutate}
+                    albumMp3File={albumMp3File}
+                    albumImgFile={albumImgFile}
+                    imgPreview={imgPreview}
+                    textInfo={textInfo}
+                    setTextInfo={setTextInfo}
+                    setAlbumImgFile={setAlbumImgFile}
+                    setAlbumMp3File={setAlbumMp3File}
+                    setImgPreview={setImgPreview}
                 />
             </ArticleWrapper>
         </React.Fragment>
